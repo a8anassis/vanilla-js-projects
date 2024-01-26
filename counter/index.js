76daa5df5ebbd28c361c8056e0c8fe25e28463cf
a8ana <a8anassis@gmail.com>
2024-01-26 16:10:20 +0200

    all

---
+++ counter/index.js
@@ -0,0 +1,124 @@
+// Initial State of the app
+const DEFAULT = 0
+let counter = DEFAULT
+
+const counterDOM = document.querySelector('#counter')
+const btns = document.querySelectorAll('.btn')
+
+/**
+ * Listener
+ */
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        if (e.currentTarget.id === 'btnDecr') {
+            onDecreaseClicked()   
+        } else if (e.currentTarget.id === 'btnIncr') {
+            onIncreaseClicked()
+        } else {
+            onResetClicked()
+        }
+    })
+})
+
+// Handlers
+
+/**
+ * Actions taken after 'Decrease' button clicked.
+ * Actions include decreasing the counter.
+ */
+function onDecreaseClicked() {
+    decreaseCounter()
+}
+
+/**
+ * Actions taken after 'Increase' button clicked.
+ * Actions include increasing the counter.
+ */
+function onIncreaseClicked() {
+    increaseCounter()
+}
+
+/**
+ * Actions taken after 'Reset' button clicked.
+ * Actions include reseting the counter.
+ */
+function onResetClicked() {
+    resetCounter(counter)
+}
+
+// Model
+
+/**
+ * Decreases the counter and syncs the UI
+ */
+function decreaseCounter() {
+    counter-- 
+    showCounter(counter)
+}
+
+/**
+ * Increases the counter and syncs the UI
+ */
+function increaseCounter() {
+    counter++ 
+    showCounter(counter)
+}
+
+/**
+ * Resets the counter and syncs the UI
+ */
+function resetCounter() {
+    counter = DEFAULT 
+    showCounter(counter)
+}
+
+// View
+
+/**
+ * Assigns counter to the corresponsing UI element
+ * and styles accordingly. 
+ * @param {Number|String} counter - the counter value
+ */
+function showCounter(counter) {
+    counterDOM.innerHTML = counter
+    styleCounter(counter)
+}
+
+/**
+ * Assigns a specific color style to the counter
+ * value. Green, when positive value, red when
+ * negative value, black when zero.
+ * 
+ * @param {Number} counter - the counter value 
+ */
+function styleCounter(counter) {
+    if (counter > 0 ) {
+        counterDOM.style.color = 'green'
+    } else if (counter < 0) {
+        counterDOM.style.color = 'red'
+    } else {
+        counterDOM.style.color = 'black'
+    }
+}
+
+
+// simple
+
+// const DEFAULT = 0
+// let counter = DEFAULT
+
+// btnDecrDOM.addEventListener('click', function() {
+//     counter--
+//     counterDOM.innerHTML = counter
+// })
+
+// btnResetDOM.addEventListener('click', function() {
+//     counter = DEFAULT
+//     counterDOM.innerHTML = counter
+// })
+
+// btnIncrDOM.addEventListener('click', function() {
+//     counter++
+//     counterDOM.innerHTML = counter
+// })
+
